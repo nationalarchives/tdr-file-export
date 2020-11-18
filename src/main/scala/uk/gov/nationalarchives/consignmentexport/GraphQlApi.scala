2189c2eb6f4e91c8257a5369bdf60201704e05b9
sam <sam.palmer@nationalarchives.gov.uk>
2020-11-18 14:17:37 +0000

    Format graphql errors and add to the exception

--- src/main/scala/uk/gov/nationalarchives/consignmentexport/GraphQlApi.scala
+++ src/main/scala/uk/gov/nationalarchives/consignmentexport/GraphQlApi.scala
@@ -4,7 +4,7 @@ import java.util.UUID
 
 import cats.effect.{ContextShift, IO}
 import cats.implicits._
-import uk.gov.nationalarchives.tdr.GraphQLClient
+import uk.gov.nationalarchives.tdr.{GraphQLClient, GraphQlResponse}
 import uk.gov.nationalarchives.tdr.keycloak.KeycloakUtils
 import graphql.codegen.GetFiles.{getFiles => gf}
 import graphql.codegen.UpdateExportLocation.{updateExportLocation => uel}
@@ -14,6 +14,7 @@ import sttp.client.{HttpURLConnectionBackend, Identity, NothingT, SttpBackend}
 import GraphQlApi._
 import io.chrisdavenport.log4cats.SelfAwareStructuredLogger
 import uk.gov.nationalarchives.consignmentexport.Config.Configuration
+import uk.gov.nationalarchives.tdr.error.GraphQlError
 
 import scala.concurrent.{ExecutionContextExecutor, Future}
 
@@ -22,24 +23,28 @@ class GraphQlApi(keycloak: KeycloakUtils,
                  updateExportLocationClient: GraphQLClient[uel.Data, uel.Variables],
                  getOriginalPathClient: GraphQLClient[gop.Data, gop.Variables])(implicit val contextShift: ContextShift[IO], val logger: SelfAwareStructuredLogger[IO]) {
 
+  implicit class ErrorUtils[D](response: GraphQlResponse[D]) {
+    val errorString: String = response.errors.map(_.message).mkString("\n")
+  }
+
   def getFiles(config: Configuration, consignmentId: UUID): IO[List[FileIdWithPath]] = for {
     token <- keycloak.serviceAccountToken(config.auth.clientId, config.auth.clientSecret).toIO
     filesResult <- filesClient.getResult(token, gf.document, gf.Variables(consignmentId).some).toIO
-    data <- IO.fromOption(filesResult.data)(new RuntimeException(s"No files found for consignment $consignmentId"))
+    data <- IO.fromOption(filesResult.data)(new RuntimeException(s"No files found for consignment $consignmentId ${filesResult.errorString}"))
     originalPath <- data.getFiles.fileIds.map(fileId => getOriginalPath(config, fileId)).sequence
   } yield originalPath
 
   def updateExportLocation(config: Configuration, consignmentId: UUID, tarPath: String): IO[Option[Int]] = for {
     token <- keycloak.serviceAccountToken(config.auth.clientId, config.auth.clientSecret).toIO
     response <- updateExportLocationClient.getResult(token, uel.document, uel.Variables(UpdateExportLocationInput(consignmentId, tarPath)).some).toIO
-    data <- IO.fromOption(response.data)(new RuntimeException(s"No data returned from the update export call for consignment $consignmentId"))
+    data <- IO.fromOption(response.data)(new RuntimeException(s"No data returned from the update export call for consignment $consignmentId ${response.errorString}"))
     _ <- logger.info(s"Export location updated for consignment $consignmentId")
   } yield data.updateExportLocation
 
   def getOriginalPath(config: Configuration, fileId: UUID) = for {
     token <- keycloak.serviceAccountToken(config.auth.clientId, config.auth.clientSecret).toIO
     response <- getOriginalPathClient.getResult(token, gop.document, gop.Variables(fileId).some).toIO
-    data <- IO.fromOption(response.data)(new RuntimeException(s"No data returned from the original path call for file id $fileId"))
+    data <- IO.fromOption(response.data)(new RuntimeException(s"No data returned from the original path call for file id $fileId ${response.errorString}"))
     originalPath <- IO.fromOption(data.getClientFileMetadata.originalPath)(new RuntimeException("The original path is missing or empty"))
   } yield FileIdWithPath(fileId, originalPath)
 }

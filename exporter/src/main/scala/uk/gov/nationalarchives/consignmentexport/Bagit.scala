cac94be56595e5a3ba002c6a589230a020b3df47
sam <sam.palmer@nationalarchives.gov.uk>
2021-02-04 19:08:03 +0000

    Add a method to write the metadata to the bag.
    Once the file-metadata.csv file has been created, the bag has to be
    updated so the checksum is added to the tag-manifest file.

--- exporter/src/main/scala/uk/gov/nationalarchives/consignmentexport/Bagit.scala
+++ exporter/src/main/scala/uk/gov/nationalarchives/consignmentexport/Bagit.scala
@@ -1,31 +1,43 @@
 package uk.gov.nationalarchives.consignmentexport
 
+import java.nio.charset.Charset
 import java.nio.file.Path
 import java.util
 import java.util.UUID
 
 import cats.effect.IO
 import gov.loc.repository.bagit.creator.BagCreator
-import gov.loc.repository.bagit.domain.Bag
+import gov.loc.repository.bagit.domain.{Bag, Manifest}
 import gov.loc.repository.bagit.hash.{StandardSupportedAlgorithms, SupportedAlgorithm}
 import gov.loc.repository.bagit.verify.BagVerifier
+import gov.loc.repository.bagit.writer.ManifestWriter
 import io.chrisdavenport.log4cats.SelfAwareStructuredLogger
+import uk.gov.nationalarchives.consignmentexport.ChecksumCalculator.ChecksumFile
 import uk.gov.nationalarchives.consignmentexport.Utils._
 
 import scala.jdk.CollectionConverters._
 import scala.language.postfixOps
 
 class Bagit(bagInPlace: (Path, util.Collection[SupportedAlgorithm], Boolean) => Bag,
-            validateBag: (Bag, Boolean) => Unit)(implicit val logger: SelfAwareStructuredLogger[IO]) {
+            validateBag: (Bag, Boolean) => Unit,
+            writeTagManifests: (util.Set[Manifest], Path, Path, Charset) => Unit
+           )(implicit val logger: SelfAwareStructuredLogger[IO]) {
 
-  def createBag(consignmentId: UUID, rootLocation: String): IO[Unit] = for {
+  def createBag(consignmentId: UUID, rootLocation: String): IO[Bag] = for {
     bag <- IO.pure(bagInPlace(s"$rootLocation/$consignmentId".toPath, List(StandardSupportedAlgorithms.SHA256: SupportedAlgorithm).asJavaCollection, true))
     _ <- IO.pure(validateBag(bag, true))
     _ <- logger.info(s"Bagit export complete for consignment $consignmentId")
-  } yield ()
+  } yield bag
+
+  def writeMetadataFilesToBag(bag: Bag, checksumFiles: List[ChecksumFile]): IO[Unit] = IO {
+    val fileToChecksumMap: util.Map[Path, String] = checksumFiles.map(f => f.file.toPath -> f.checksum).toMap.asJava
+    bag.getTagManifests.asScala.head.getFileToChecksumMap.putAll(fileToChecksumMap)
+    writeTagManifests.apply(bag.getTagManifests, bag.getRootDir, bag.getRootDir, bag.getFileEncoding)
+  }
 }
 
 object Bagit {
   //Passing the method value to the class to make unit testing possible as there's no easy way to mock the file writing
-  def apply()(implicit logger: SelfAwareStructuredLogger[IO]): Bagit = new Bagit(BagCreator.bagInPlace, new BagVerifier().isComplete)(logger)
+  def apply()(implicit logger: SelfAwareStructuredLogger[IO]): Bagit =
+    new Bagit(BagCreator.bagInPlace, new BagVerifier().isComplete, ManifestWriter.writeTagManifests)(logger)
 }
